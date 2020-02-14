24fe5a5d6f71403b66a20918606c42227fecadb1
JuhoL32 <juho.leskela@student.lab.fi>
2020-02-14 12:45:17 +0200

    Initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <CommitChart></CommitChart>
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
@@ -8,11 +9,12 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
-
+import CommitChart from '@/components/CommitChart.vue';
 export default {
   name: "Home",
   components: {
     HelloWorld
   }
+  
 };
 </script>
